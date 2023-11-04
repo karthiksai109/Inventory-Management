5c8aefa22733449c9ab445becca4c7a201616b19
karthiksai109 <karthiksaidham109@gmail.com>
2023-11-04 14:36:14 +0530

    second commit

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -9,22 +9,21 @@ const shortid = require('shortid')
 const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
 
 const createShortUrl=async function(req,res){
+    res.setHeader("Access-Control-Allow-Origin","*")
     try{
     let body=req.body
     let data={}
+    console.log(body)
    let obj=Object.keys(body)
    if(obj.length==0)return  res.status(400).send({status:false,message:'please enter longurl'})
    if(obj.length>1) return res.status(400).send({status:false,message:'please remove attributes other than longurl'})
    if(!obj.includes('longUrl')) return res.status(400).send({status:false,message:'longurl is required'})
-   if(typeof(body.longUrl)!="string" || body.longUrl.trim()==""){
+   if(body.longUrl.trim()==""){
     return res
       .status(400)
-      .send({ status: false, message: `longUrl cant be empty or a wrong data type  🤦‍♂️ `});
+      .send({ status: false, message: `longUrl cant be empty`});
   }
    let longUrl=body['longUrl']
-  
-  
-    
    
   let x=await urlModel.findOne({"longUrl":body['longUrl']})
 console.log(x)
@@ -34,14 +33,14 @@ if (!regex.test(longUrl.trim())) {
       .send({ status: false, message: "please enter validUrl" });
   }
 
-if(x) return res.status(200).send({ status: true, message: `shorturl for this url is already generated `});
+if(x) return res.status(200).send({ status: true, message: `shorturl for this url is already generated `,urlCode:x.urlCode});
   let urlcode=shortid.generate(longUrl)
   let shortUrl=baseUrl+urlcode.toLowerCase()
   data['urlCode']=urlcode.toLowerCase()
   data['longUrl']=longUrl.trim()
   data['shortUrl']=shortUrl
 let createdata=await urlModel.create(data)
-return res.status(201).send({status:true,message:'shorturl succesfully created'})
+return res.status(201).send({status:true,message:'shorturl succesfully created',urlCode:data.urlCode})
     }
 catch(err){
     return res.status(500).send({status:false,message:err.message})
@@ -52,7 +51,8 @@ module.exports.createShortUrl=createShortUrl
 
 
 const getUrl=async function(req,res){
-    try{
+     res.setHeader("Access-Control-Allow-Origin","*")
+     try{
         let urlCode=req.params['urlCode']
         if(!shortid.isValid(urlCode.trim())) return res.status(400).send({status:false,message:'please enter valid urlcode'})
         let y=await urlModel.findOne({'urlCode':urlCode})
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express=require('express')
 const mongoose=require('mongoose')
+const cors=require('cors')
 const route=require('./Routes/router')
+const path=require('path')
 const app=express()
-
 app.use(express.json())
+app.use(cors())
+
+
 mongoose.set('strictQuery',true)
 mongoose.connect('mongodb+srv://group21Database:f8HsIED1oiOyc6yi@karthikcluster.b2ikjot.mongodb.net/Register',{
     useNewUrlParser:true,
+   
 }).then(()=>console.log('MongoDb connected'))
 .catch((err)=>console.log(err))
+
 app.use('/',route)
 
-app.listen(process.env.PORT || 3000,function(){
-    console.log(`connected to port ${process.env.PORT || 3000}`)
+
+app.listen(process.env.PORT || 5000,function(){
+    console.log(`connected to port ${process.env.PORT || 5000}`)
 })
